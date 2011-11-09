03785f437f8f3df64987d4cfef4c62ba440717af
Luke Slater <tinmachin3@gmail.com>
2011-11-09 21:34:10 +0000

    new jsbot

--- jsbot.js
+++ jsbot.js
@@ -1,117 +1,151 @@
-require('prototype');
 var sys = require('sys');
 var net = require('net');
 
-var JSBot = Class.create({
-    initialize: function(nick, host, port, onReady) {
-        this.nick = nick;
-        this.host = host;
-        this.port = port;
-        this.encoding = 'utf8';
-        this.lineBuffer = '';
-        this.onReady = onReady;
-        this.events = {
-            'JOIN': [],
-            'PART': [],
-            'KICK': [],
-            'PRIVMSG': []
-        };
+var JSBot = function(nick, host, port, owner, onReady) {
+    this.nick = nick;
+    this.host = host;
+    this.port = port;
+    this.owner = owner;
+    this.channels = [];
+    this.commands = {};
+    this.encoding = 'utf8';
+    this.lineBuffer = '';
+    this.onReady = onReady;
+    this.events = {
+        'JOIN': [],
+        'PART': [],
+        'KICK': [],
+        'PRIVMSG': [],
+        'MODE': []
+    };
+};
+
+JSBot.prototype.inChannel = function(channel) {
+    if(this.channels.include(channel)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+JSBot.prototype.connect = function() {
+    this.conn = net.createConnection(this.port, this.host);
+    this.conn.setTimeout(60 * 60 * 1000);
+    this.conn.setEncoding(this.encoding);
+
+    this.conn.addListener('connect', function() {
+        this.send('NICK', this.nick);
+        this.send('USER', this.nick, '0', '*', this.nick);
+        this.onReady();
+    }.bind(this));
+
+    this.conn.addListener('data', function(chunk) {
+        this.lineBuffer += chunk;
+        if(chunk.endsWith('\r\n')) {
+            this.parse();
+            this.lineBuffer = '';
+        }
+    }.bind(this));
+
+    this.conn.addListener('end', function() {
         this.connect();
-    },
+    }.bind(this));
+};
 
-    connect: function() {
-        this.conn = net.createConnection(this.port, this.host);
-        this.conn.setEncoding(this.encoding);
+JSBot.prototype.send = function() {
+    var message = [].splice.call(arguments, 0).join(' ');
+    message += '\r\n';
+    this.conn.write(message, this.encoding);
+};
 
-        this.conn.addListener('connect', function() {
-            this.send('NICK', this.nick);
-            this.send('USER', this.nick, '0', '*', this.nick);
-            this.onReady();
-        }.bind(this));
+JSBot.prototype.join = function(channel) {
+    this.send('JOIN', channel);
+    this.channels.push(channel);
+};
 
-        this.conn.addListener('data', function(chunk) {
-            this.lineBuffer += chunk;
-            if(chunk.endsWith('\r\n')) {
-                this.parse();
-                this.lineBuffer = '';
-            }
-        }.bind(this));
-    },
+JSBot.prototype.part = function(channel) {
+    this.send('PART', channel);
+    this.channels.remove(channel);
+};
 
-    send: function() {
-        message = [].splice.call(arguments, 0).join(' ');
-        message += '\r\n';
-        this.conn.write(message, this.encoding);
-    },
+JSBot.prototype.say = function(channel, message) {
+    this.send('PRIVMSG', channel, ':' + message);
+};
 
-    join: function(channel) {
-        this.send('JOIN', channel);
-    },
-
-    part: function(channel) {
-        this.send('PART', channel);
-    },
-
-    say: function(channel, message) {
-        this.send('PRIVMSG', channel, ':' + message);
-    },
-
-    parse: function() {
-        if(this.lineBuffer.startsWith('PING')) {
-            this.pong(this.lineBuffer);
-        } else {
-            var message = this.lineBuffer.match(/(?:(:[^\s]+) )?([^\s]+) (.+)/);
-            var prefix = message[1];
-            var command = message[2];
-            var parameters = message[3];
+JSBot.prototype.parse = function() {
+    if(this.lineBuffer.startsWith('PING')) {
+        this.pong(this.lineBuffer);
+    } else {
+        var message = this.lineBuffer.match(/(?:(:[^\s]+) )?([^\s]+) (.+)/);
+        var prefix = message[1];
+        var command = message[2];
+        var parameters = message[3];
+        try {
             var data = {
                 'raw': message,
                 'user': prefix.split('!')[0].substring(1)
             };
-
-            // TODO: Further regex to avoid all this stringwork?
-            switch(command) {
-                case 'JOIN': case 'PART':
-                    data['channel'] = parameters.split(':')[1];
-                    break;
-
-                case 'PRIVMSG':
-                    var colonSplit = parameters.split(':');
-                    data['channel'] = parameters.split(' ')[0];
-                    data['message'] = colonSplit.slice(1, colonSplit.length).join(':');
-                    break;
-
-                case 'KICK':
-                    data['channel'] = parameters.split(' ')[0];
-                    data['kickee'] = parameters.split(' ')[1];
-                    break;
-            }
-
-            if(command in this.events) {
-                this.events[command].each(function(eventFunc) {
-                    if(Object.isFunction(eventFunc)) {
-                        eventFunc(data);
-                    }
-                });
-            }
-
-            // DEBUG
-            console.log('line: ' + message[0]);
-            console.log('prefix: ' + message[1]);
-            console.log('command: ' + message[2]);
-            console.log('params: ' + message[3]);
+        } catch(err) {
+            var data = {
+                'raw': message,
+                'user': 'none'
+            };
         }
-    },
 
-    pong: function(message) {
-        this.send('PONG', ':' + message.split(':')[1]);
-    },
+        // TODO: Further regex to avoid all this stringwork?
+        switch(command) {
+            case 'JOIN': case 'PART': case 'MODE':
+                data['channel'] = parameters.split(':')[1];
+                break;
 
-    addListener: function(index, func) {
-        this.events[index].push(func);
+            case 'PRIVMSG':
+                var colonSplit = parameters.split(':');
+                data['channel'] = parameters.split(' ')[0];
+                data['message'] = colonSplit.slice(1, colonSplit.length).join(':');
+                break;
+
+            case 'KICK':
+                data['channel'] = parameters.split(' ')[0];
+                data['kickee'] = parameters.split(' ')[1];
+                break;
+        }
+
+        if(command in this.events) {
+            this.events[command].each(function(eventFunc) {
+                if(Object.isFunction(eventFunc)) {
+                    try {
+                        eventFunc.call(this.owner, data);
+                    } catch(err) {
+                        console.log('ERROR: ' + eventFunc + '\n' + err);
+                    }
+                }
+            });
+        }
+
+        // DEBUG
+        console.log('line: ' + message[0]);
     }
-});
+};
 
-exports.createJSBot = function(nick, host, port, onReady) {
-    return new JSBot(nick, host, port, onReady);
+JSBot.prototype.pong = function(message) {
+    this.send('PONG', ':' + message.split(':')[1]);
+    console.log('PONG');
+};
+
+JSBot.prototype.addListener = function(index, func) {
+    this.events[index].push(func);
+};
+
+JSBot.prototype.removeListeners = function() {
+    this.events = {
+        'JOIN': [],
+        'PART': [],
+        'KICK': [],
+        'PRIVMSG': [],
+        'MODE': []
+    };
+};
+
+exports.createJSBot = function(nick, host, port, owner, onReady) {
+    return new JSBot(nick, host, port, owner, onReady);
 };
